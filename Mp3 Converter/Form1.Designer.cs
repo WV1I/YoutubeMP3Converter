a502ab2ceb4ff54ccd605502fe0cdc4b2efc60a9
WV1I <75336721+WV1I@users.noreply.github.com>
2024-05-25 21:26:36 +0200

    Initial Release

---
+++ Mp3 Converter/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace Mp3_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.downbutton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.types = new System.Windows.Forms.ListBox();
+            this.patherror = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // downbutton
+            // 
+            this.downbutton.Location = new System.Drawing.Point(389, 181);
+            this.downbutton.Name = "downbutton";
+            this.downbutton.Size = new System.Drawing.Size(99, 25);
+            this.downbutton.TabIndex = 0;
+            this.downbutton.Text = "Download";
+            this.downbutton.UseVisualStyleBackColor = true;
+            this.downbutton.Click += new System.EventHandler(this.downbutton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(110, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(388, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(100, 181);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 25);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Choose Folder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(110, 52);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(388, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(249, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Downloaded 0%";
+            // 
+            // types
+            // 
+            this.types.FormattingEnabled = true;
+            this.types.Items.AddRange(new object[] {
+            "mp4",
+            "mp3",
+            "ogg",
+            "wav"});
+            this.types.Location = new System.Drawing.Point(243, 150);
+            this.types.Name = "types";
+            this.types.Size = new System.Drawing.Size(90, 56);
+            this.types.TabIndex = 6;
+            // 
+            // patherror
+            // 
+            this.patherror.AutoSize = true;
+            this.patherror.ForeColor = System.Drawing.Color.Red;
+            this.patherror.Location = new System.Drawing.Point(107, 165);
+            this.patherror.Name = "patherror";
+            this.patherror.Size = new System.Drawing.Size(83, 13);
+            this.patherror.TabIndex = 7;
+            this.patherror.Text = "Path not Found!";
+            this.patherror.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(82, 49);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(591, 233);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.patherror);
+            this.Controls.Add(this.types);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.downbutton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button downbutton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox types;
+        private System.Windows.Forms.Label patherror;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Mp3 Converter/Form1.cs
@@ -0,0 +1,139 @@
+﻿using MediaToolkit;
+using MediaToolkit.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VideoLibrary;
+
+namespace Mp3_Converter
+{
+    public partial class Form1 : Form
+    {
+
+        char[] invalidFileChars = Path.GetInvalidFileNameChars();
+        string path;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private  void konvert(string fileName, string directory)
+        {
+            var type = types.SelectedItem;
+            if (type == "mp4") return;
+
+            var Convert = new NReco.VideoConverter.FFMpegConverter();
+            string fullname = directory + fileName;
+            String SaveMP3File = fullname.Replace(".mp4", $".{type}");
+            Convert.ConvertMedia(fullname, SaveMP3File, $"{type}");
+            //Delete the MP4 file after conversion
+            File.Delete(fullname);
+        }
+
+
+
+        private async void Download(string SaveToFolder, string VideoURL, char [] charArray)
+        {
+            label1.Text = "Download Started";
+
+            var source = @SaveToFolder += "/";
+            var youtube = YouTube.Default;
+            var video = youtube.GetVideo(VideoURL);
+            var client = new HttpClient();
+
+            long? totalByte = 0;
+
+            string fileExtension = video.FileExtension;
+
+            var fileName = $"{video.Title}{fileExtension}";
+            foreach (char x in charArray)
+            {
+                if (fileName.Contains(x))
+                {
+                    // Sprawdzanie czy nazwa nie zawiera niewłaściwych znaków
+                    fileName = fileName.Replace(x.ToString(), "");
+
+                }
+            }
+            label1.Text = "Downloading...";
+            using (Stream output = File.OpenWrite(source + fileName))
+            {
+                using (var request = new HttpRequestMessage(HttpMethod.Head, video.Uri))
+                {
+                    totalByte = client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead).Result.Content.Headers.ContentLength;
+                }
+                using (var input = await client.GetStreamAsync(video.Uri))
+                {
+                    byte[] buffer = new byte[16 * 1024];
+                    int read;
+                    int totalRead = 0;
+                    int size = Convert.ToInt32(totalByte);
+                    progressBar1.Maximum = size;
+                    
+                    
+                    while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        output.Write(buffer, 0, read);
+                        totalRead += read;
+                        progressBar1.Value = totalRead;
+                        
+                    }
+
+                    
+                }
+                
+            }
+
+
+
+            konvert(fileName, source);
+            label1.Text = "Download Complete";
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void downbutton_Click(object sender, EventArgs e)
+        {
+            if (path == "")
+            {
+                patherror.Visible = true;
+                return;
+            }
+            else
+                patherror.Visible = false;
+            Download(path, textBox1.Text, invalidFileChars);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                path = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if(textBox1.Text.Contains("www.youtube.com"))
+            {
+
+            }
+        }
+    }
+}
